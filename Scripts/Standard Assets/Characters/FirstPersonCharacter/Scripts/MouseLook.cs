73adcba5c6a9b76266ecb63fe499d19297187697
GintasS <svedgintas@gmail.com>
2023-12-20 15:35:21 +0200

    Added standard asset scripts.

---
+++ Scripts/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -0,0 +1,80 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.FirstPerson
+{
+    [Serializable]
+    public class MouseLook
+    {
+        [Header("Mouse Sensitivity X Settings")]
+        public float XSensitivity = 2f;
+        public float MinimumX = -90F;
+        public float MaximumX = 90F;
+        [Header("Mouse Sensitivity Y Settings")]
+        public float YSensitivity = 2f;
+        public bool clampVerticalRotation = true;
+        [Header("Smooth Settings")]
+        public bool smooth;
+        public float smoothTime = 5f;
+
+        private Quaternion m_CharacterTargetRot;
+        private Quaternion m_CameraTargetRot;
+
+        public void Init(Transform character, Transform camera)
+        {
+            m_CharacterTargetRot = character.localRotation;
+            m_CameraTargetRot = camera.localRotation;
+        }
+
+        public void LookRotation(Transform character, Transform camera)
+        {
+            var yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
+            var xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
+
+            m_CharacterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
+            m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+
+            if (clampVerticalRotation)
+            {
+                m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
+            }
+
+            if(smooth)
+            {
+                character.localRotation = Quaternion.Slerp(character.localRotation, m_CharacterTargetRot,
+                    smoothTime * Time.deltaTime);
+                camera.localRotation = Quaternion.Slerp(camera.localRotation, m_CameraTargetRot,
+                    smoothTime * Time.deltaTime);
+            }
+            else
+            {
+                character.localRotation = m_CharacterTargetRot;
+                camera.localRotation = m_CameraTargetRot;
+            }
+        }
+
+        public void InvertMouse(bool? value)
+        {
+            if (value.HasValue && value.Value)
+            {
+                XSensitivity *= -1;
+                YSensitivity *= -1;
+            }
+        }
+
+        private Quaternion ClampRotationAroundXAxis(Quaternion q)
+        {
+            q.x /= q.w;
+            q.y /= q.w;
+            q.z /= q.w;
+            q.w = 1.0f;
+
+            var angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
+            angleX = Mathf.Clamp (angleX, MinimumX, MaximumX);
+
+            q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
+            return q;
+        }
+    }
+}
